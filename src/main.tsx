b2731595208099515461d72433d5c75f60670605
ZinnyEdmund <edmunddebby24@gmail.com>
2025-03-09 04:15:53 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import List from './list'
+import List from './List'
 
 const Main: React.FC = () => {
   return (
